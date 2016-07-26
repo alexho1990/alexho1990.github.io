0ba05cd3c8b549aef2b1b454ed2140a7fde09ee5
Adam Lancaster <lancaster28@hotmail.com>
2016-07-26 23:47:12 +0100

    Setup

---
+++ assets/js/carousel.js
@@ -0,0 +1,23 @@
+// window.onload = window.setTimeout(firstImage, 1000);
+
+// var carousel = document.getElementById("carousel").style
+
+// function firstImage() {
+//   carousel.background-image="url('assets/images/first.png') no-repeat center center";
+//   setTimeout(secondImage, 10000);
+// };
+
+// function secondImage() {
+//   carousel.background="url('assets/images/second.png') no-repeat center center";
+//   setTimeout(thirdImage, 10000);
+// };
+
+// function thirdImage () {
+//   carousel.background="url('assets/images/third.png') no-repeat center center";
+//   setTimeout(fourthImage, 10000);
+// };
+
+// function fourthImage () {
+//   carousel.background="url('assets/images/fourth.png') no-repeat center center";
+//   setTimeout(firstImage, 10000);
+// };
