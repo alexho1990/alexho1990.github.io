a05b9eefa044ef76a9d5544c6b4e74287d46611c
Adam Lancaster <lancaster28@hotmail.com>
2016-07-27 20:46:08 +0100

    Carousel kind of working, transition a little abrupt

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,23 +1,26 @@
-// window.onload = window.setTimeout(firstImage, 1000);
+window.onload = window.setTimeout(firstImage, 500);
 
-// var carousel = document.getElementById("carousel").style
+var carousel = document.getElementById("carousel-img")
 
-// function firstImage() {
-//   carousel.background-image="url('assets/images/first.png') no-repeat center center";
-//   setTimeout(secondImage, 10000);
-// };
+function firstImage() {
+  carousel.setAttribute("src", "./assets/images/first.png");
+  setTimeout(secondImage, 5000);
+};
 
-// function secondImage() {
-//   carousel.background="url('assets/images/second.png') no-repeat center center";
-//   setTimeout(thirdImage, 10000);
-// };
+function secondImage() {
+  carousel.setAttribute("src", "./assets/images/second.png");
+  setTimeout(thirdImage, 5000);
+};
 
-// function thirdImage () {
-//   carousel.background="url('assets/images/third.png') no-repeat center center";
-//   setTimeout(fourthImage, 10000);
-// };
+function thirdImage () {
+  carousel.setAttribute("src", "./assets/images/third.png");
+  setTimeout(fourthImage, 5000);
+};
 
-// function fourthImage () {
-//   carousel.background="url('assets/images/fourth.png') no-repeat center center";
-//   setTimeout(firstImage, 10000);
-// };
+function fourthImage () {
+  carousel.setAttribute("src", "./assets/images/fourth.png");
+  setTimeout(firstImage, 5000);
+};
+  
+
+  "url('assets/images/first.png') no-repeat center center";
